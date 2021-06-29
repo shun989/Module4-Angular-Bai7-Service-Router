980645ed243a342d02d90cb987189c55f14bdeae
kibo <minhtuanvt89@gmail.com>
2021-06-29 10:25:03 +0700

    up project

---
+++ angular-product-manager/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductCreateComponent } from './products/product-create/product-create.component';
+import { ProductListComponent } from './products/product-list/product-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductCreateComponent,
+    ProductListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
